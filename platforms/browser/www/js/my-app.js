ab6862735ada6aebd0407d94a8a7098a51d5a42f
alphapyxidis <vincent.vermorel@gmail.com>
2017-01-05 16:24:51 +0100

    GPS watch

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -42,20 +42,6 @@ $$(document).on('deviceready', function() {
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way)
 
-myApp.onPageInit('about', function (page) {
-    
-console.log('Cordova : ' + device.cordova);
-console.log('Model : ' + device.model);
-console.log('Platform : ' + device.platform);
-console.log('Uuid : ' + device.uuid);
-console.log('Version : ' + device.version);
-console.log('Manufacturer : ' + device.manufacturer);
-console.log('IsVirtual : ' + device.isVirtual);
-console.log('Serial : ' + device.serial);
-
-})
-
-
  myApp.onPageInit('qrcode', function (page) {
     cordova.plugins.barcodeScanner.scan(
       function (result) {
@@ -75,47 +61,51 @@ console.log('Serial : ' + device.serial);
    );
 }) 
 
+myApp.onPageBack('gps', function (page) {
+
+    // arrête l'acquisition du GPS
+    if (watchID != -1) {
+        navigator.geolocation.clearWatch(watchID); 
+        watchID = -1;
+    }
+})
+
 myApp.onPageInit('gps', function (page) {
     // onSuccess Callback
     // This method accepts a Position object, which contains the
     // current GPS coordinates
     //
     var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
+        
+        // alert('Latitude: '          + position.coords.latitude          + '\n' +
+              // 'Longitude: '         + position.coords.longitude         + '\n' +
+              // 'Altitude: '          + position.coords.altitude          + '\n' +
+              // 'Accuracy: '          + position.coords.accuracy          + '\n' +
+              // 'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              // 'Heading: '           + position.coords.heading           + '\n' +
+              // 'Speed: '             + position.coords.speed             + '\n' +
+              // 'Timestamp: '         + position.timestamp                + '\n');
+        
+        var d = new Date(position.timestamp);
+
+        document.getElementById('tracegps').innerHTML = document.getElementById('tracegps').innerHTML + '<br>' + d.toISOString() + '&nbsp;&nbsp;&nbsp;&nbsp;' + position.coords.latitude + '&nbsp;&nbsp;&nbsp;&nbsp;' + position.coords.longitude ;
     };
 
     // onError Callback receives a PositionError object
     //
     function onError(error) {
-        alert('code: '    + error.code    + '\n' +
+        alert('GPS ERROR\n\ncode: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
     }
 
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-})
-
-
-myApp.onPageBack('compass', function (page) {
-
-    // ... later on ...
-    if (watchID != -1) {
-        navigator.compass.clearWatch(watchID);
-        watchID = -1;
-    }
+    watchID = navigator.geolocation.watchPosition(onSuccess, onError, { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }); 
 })
 
 myApp.onPageInit('camera', function (page) {
     navigator.camera.getPicture (
         function (imageData) {
                 var image = document.getElementById('myImage') ;
-                image.src = "données : image / jpeg ; base64," + imageData;
+                image.src = "data:image/jpeg;base64," + imageData;
         }, 
         function (message) 
         {
@@ -127,6 +117,43 @@ myApp.onPageInit('camera', function (page) {
             destinationType: Camera.DestinationType.FILE_URI
         }
     );
+})
+
+myApp.onPageInit('sysinfo', function (page) {
+    var dt = new Date(Date.now());
+    
+console.log('Cordova : ' + device.cordova);
+console.log('Model : ' + device.model);
+console.log('Platform : ' + device.platform);
+console.log('Uuid : ' + device.uuid);
+console.log('Version : ' + device.version);
+console.log('Manufacturer : ' + device.manufacturer);
+console.log('IsVirtual : ' + device.isVirtual);
+console.log('Serial : ' + device.serial);
+
+    document.getElementById('model').innerHTML = device.model;
+    document.getElementById('platform').innerHTML = device.platform;
+    document.getElementById('version').innerHTML = device.version;
+        
+    document.getElementById('localdate').innerHTML =   dt.getFullYear() +"-"+
+        ("0" + (dt.getMonth()+1)).slice(-2) +"-"+
+        ("0" + dt.getDate()).slice(-2) + "T" +
+        ("0" + dt.getHours()).slice(-2) + ":" +
+        ("0" + dt.getMinutes()).slice(-2) + ":" +
+        ("0" + dt.getSeconds()).slice(-2) + "." +
+        ("00" + dt.getMilliseconds()).slice(-3);
+    document.getElementById('UTCdate').innerHTML = dt.toISOString();
+    
+})
+
+
+myApp.onPageBack('compass', function (page) {
+
+    // arrête l'acquisition de la boussole
+    if (watchID != -1) {
+        navigator.compass.clearWatch(watchID);
+        watchID = -1;
+    }
 })
 
 myApp.onPageInit('compass', function (page) {
